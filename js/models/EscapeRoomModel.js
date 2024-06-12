62f73ee71de978bfbd3a4f4f8a121c0c3f473963
AndreRodrigues884 <andreprodrigues0512@gmail.com>
2024-06-12 20:23:34 +0100

    Front-End Escape Room

---
+++ js/models/EscapeRoomModel.js
@@ -0,0 +1,109 @@
+class EscapeRoom {
+    constructor(modalId) {
+        this.modalId = modalId;
+        this.modalInstance = new bootstrap.Modal(document.getElementById(this.modalId));
+        this.objects = Array.from(document.querySelectorAll('.group-2 img'));
+        this.tvObjects = Array.from(document.querySelectorAll('.pc-group img'));
+        this.clickedObjectId = null;
+        this.changedImagesCount = 0;
+        this.counterDisplay = document.getElementById('counter');
+        this.fiveMinutes = 60 * 3;
+        this.objects.forEach(object => {
+            object.addEventListener('click', () => {
+                this.handleObjectClick(object);
+            });
+        });
+        this.tvObjects.forEach(tv => {
+            tv.addEventListener('click', () => {
+                this.handleTVClick(tv);
+            });
+        });
+        this.startCounter(this.fiveMinutes, this.counterDisplay, () => {
+            alert('Você perdeu o jogo!');
+            location.reload();
+        });
+    }
+
+    handleObjectClick(object) {
+        console.log('Objeto clicado:', object.id);
+        this.clickedObjectId = object.id;
+    }
+
+    handleTVClick(tv) {
+        console.log('TV clicada:', tv.id);
+    }
+
+    showModal() {
+        this.modalInstance.show();
+    }
+
+    hideModal() {
+        this.modalInstance.hide();
+    }
+
+    verifyAnswer() {
+        let isBlueChecked = document.getElementById('checkBlue').checked;
+        let isRedChecked = document.getElementById('checkRed').checked;
+
+        if (isBlueChecked && !isRedChecked) {
+            alert('Resposta correta!');
+            if (this.clickedObjectId) { // Verifica se há um objeto clicado
+                this.changeTVImage(this.clickedObjectId); // Altera a imagem da TV com base no objeto clicado
+                this.changedImagesCount++; // Incrementa o contador de imagens alteradas
+                if (this.changedImagesCount === this.tvObjects.length) { // Verifica se todas as imagens foram alteradas
+                    alert('Você ganhou o jogo!'); // Exibe o alerta de vitória
+                    location.reload();
+                }
+            }
+        } else {
+            alert('Resposta errada! A resposta correta é Azul.');
+        }
+        this.hideModal();
+    }
+
+    changeTVImage(objectId) {
+        switch (objectId) {
+            case 'object1':
+                document.getElementById('tv-1').src = "/img/tv1_check.png"; // Altere o caminho da nova imagem aqui
+                break;
+            case 'object3':
+                document.getElementById('tv-2').src = "/img/tv2_check.png"; // Altere o caminho da nova imagem aqui
+                break;
+            case 'object4':
+                document.getElementById('tv-3').src = "/img/tv3_check.png"; // Altere o caminho da nova imagem aqui
+                break;
+            case 'object5':
+                document.getElementById('tv-4').src = "/img/tv4_check.png"; // Altere o caminho da nova imagem aqui
+                document.getElementById('tv-5').src = "/img/tv5_check.png"; // Altere o caminho da nova imagem aqui
+                break;
+            case 'object6':
+                document.getElementById('tv-6').src = "/img/tv6_check.png"; // Altere o caminho da nova imagem aqui
+                break;
+                case 'object7':
+                    document.getElementById('tv-7').src = "/img/tv7_check.png";
+                    break;
+            default:
+                break;
+        }
+    }
+    
+    startCounter(duration, display, callback) {
+        let timer = duration, minutes, seconds;
+        const intervalId = setInterval(() => {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                clearInterval(intervalId);
+                callback();
+            }
+        }, 1000);
+    }
+}
+
+export default EscapeRoom;
--- js/views/user/EscapeRoomView.js
+++ js/views/user/EscapeRoomView.js
@@ -1,28 +1,58 @@
-function escapeMessage() {
-    let loginStatus = localStorage.getItem('isLoggedIn');
-    let nameUser = localStorage.getItem('nameUser');
-    let userRole = localStorage.getItem('userRole');
-    let escapeMessage = document.getElementById('escapeMessage');
+import EscapeRoom from '../../models/EscapeRoomModel.js';
 
-    if (escapeMessage) {
-        if (userRole === 'user' && loginStatus === 'true') {
-            escapeMessage.textContent += 'Bem-vindo ao Escape Room, ' + nameUser + '!';
-            let startButton = document.createElement('button');
-            startButton.textContent = 'Iniciar';
-            startButton.addEventListener('click', function () {
-                window.location.href = '/html/user/StartEscapeRoom.html';
-            });
-            escapeMessage.appendChild(startButton);
-        } else {
-            escapeMessage.textContent += 'Precisa de iniciar sessão para participar neste Escape Room!';
-            let loginButton = document.createElement('button');
-            loginButton.textContent = 'Ir para Login';
-            loginButton.addEventListener('click', function () {
-                window.location.href = '/html/LoginView.html';
-            });
-            escapeMessage.appendChild(loginButton);
-        }
-    };
-}
+const object1 = document.getElementById('object1');
+const object3 = document.getElementById('object3');
+const object4 = document.getElementById('object4');
+const object5 = document.getElementById('object5');
+const object6 = document.getElementById('object6');
+const object7 = document.getElementById('object7');
+const btnVerify = document.getElementById('btnVerify');
 
-escapeMessage()
+
+let clickedObjectId;
+
+object1.addEventListener('click', () => {
+    clickedObjectId = 'object1';
+    modalHandler.showModal();
+});
+
+object3.addEventListener('click', () => {
+    clickedObjectId = 'object3';
+    modalHandler.showModal();
+});
+
+object4.addEventListener('click', () => {
+    clickedObjectId = 'object4';
+    modalHandler.showModal();
+});
+
+object5.addEventListener('click', () => {
+    clickedObjectId = 'object5';
+    modalHandler.showModal();
+});
+
+object6.addEventListener('click', () => {
+    clickedObjectId = 'object6';
+    modalHandler.showModal();
+});
+
+object7.addEventListener('click', () => {
+    clickedObjectId = 'object7';
+    modalHandler.showModal();
+});
+
+
+
+const modalHandler = new EscapeRoom('modalQuestion');
+
+object1.addEventListener('click', () => modalHandler.showModal());
+object3.addEventListener('click', () => modalHandler.showModal());
+object4.addEventListener('click', () => modalHandler.showModal());
+object5.addEventListener('click', () => modalHandler.showModal());
+object6.addEventListener('click', () => modalHandler.showModal());
+object7.addEventListener('click', () => modalHandler.showModal());
+btnVerify.addEventListener('click', () => modalHandler.verifyAnswer(clickedObjectId));
+
+
+
+
